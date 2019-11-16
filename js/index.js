3656fa758c8a6b35d4d31b747ae1d57624b87938
Holden Russell <holdenrussell@Holdens-MacBook-Pro.local>
2019-11-16 14:48:42 -0700

    fixed responsive nav

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-    var x = document.getElementsByClassName("nav");
+    var x = document.getElementById("navbar");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
+      
     }
   }
